4669522ee9d2bb0babf5edd3bc73171e3f3fa637
Obsidian-lava <git.aggregate559@slmails.com>
2026-05-05 23:44:11 +0500

    Added sqlite

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,5 @@
 from app.schemas import TaskSchema
+from app.database import get_db
 
 db = {
     "2": {"title": "...",
@@ -8,13 +9,31 @@ db = {
 }
 
 def get_tasks():
-    return db
+    conn = get_db()
+
+    cursor = conn.execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+
+    conn.close()
+    return {row['id']: {"title": row['title'], "description": row['description']} for row in rows}
 
 def add_task(id: str, task: TaskSchema):
-    db[id] = task.dict()
+    conn = get_db()
+
+    conn.execute(
+        "INSERT INTO tasks (id, title, description) VALUES (?, ?, ?)",
+        (id, task.title, task.description))
+    conn.commit()
+    conn.close()
 
 def delete_tasks(id: str):
-    if id in db:
-        del db[id]
+    conn = get_db()
+
+    cur = conn.execute("SELECT id FROM tasks WHERE id = ?", (id,))
+    if cur.fetchone():
+        conn.execute("DELETE FROM tasks WHERE id = ?", (id,))
+        conn.commit()
+        conn.close()
         return {"message": f"Task {id} deleted"}
+    conn.close()
     return {"error": "task not found"}, 404
--- app/server.py
+++ app/server.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from app.routers import router as tasks_router
+from app.database import get_db
 import uvicorn
+
+
+conn = get_db()
+conn.execute("CREATE TABLE IF NOT EXISTS tasks (id TEXT PRIMARY KEY, title TEXT, description TEXT)")
+
+cursor = conn.execute("SELECT COUNT(*) FROM tasks")
+if cursor.fetchone()[0] == 0:
+    conn.execute("INSERT INTO tasks VALUES (?, ?, ?)", ("2", "Default Task 2", "Description 2"))
+    conn.execute("INSERT INTO tasks VALUES (?, ?, ?)", ("4", "Default Task 4", "Description 4"))
+    conn.commit()
+
+conn.close()
 
 app = FastAPI()
 app.include_router(tasks_router)
