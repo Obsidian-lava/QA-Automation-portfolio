495100d85b3a0c5df634b0dde9a6af8beb34e0fb
Obsidian-lava <git.aggregate559@slmails.com>
2026-05-08 23:37:21 +0500

    New test for PUT method

--- tests/test_server.py
+++ tests/test_server.py
@@ -21,3 +21,18 @@ def test_delete():
     data = response.json()
 
     assert task_id not in data, "Эта задача все еще присутствует в базе"
+
+def test_put():
+    task_id = "4"
+
+    put_url = f"{url}/{task_id}"
+
+    new_data = {"title": "special task", "description": "this is special task"}
+
+    response_put = requests.put(put_url, json=new_data)
+
+    response = requests.get(url)
+    data = response.json()
+
+    assert data[task_id]["title"] == new_data["title"]
+    assert data[task_id]["description"] == new_data["description"]
